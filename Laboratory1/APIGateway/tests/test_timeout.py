b6517070bb5780d14aefc3f1c8720ff77ceb7524
Gherman Adrian <valentina.craevscaia@isa.utm.md>
2023-10-21 23:37:54 +0300

    Started creating docker files

--- Laboratory1/APIGateway/tests/test_timeout.py
+++ Laboratory1/APIGateway/tests/test_timeout.py
@@ -6,7 +6,7 @@ from time import sleep
 import requests
 import requests_mock
 
-from apiGateway import SERVICE_DISCOVERY_URL
+import gatewayConfig
 
 # Your gateway's configuration
 GATEWAY_URL = 'http://localhost:5000'
@@ -47,7 +47,7 @@ class TestTimeout(unittest.TestCase):
         mocked_service_address = f'http://localhost:{DELAY_SERVER_PORT}'
 
         with requests_mock.Mocker() as m:
-            m.get(f'{SERVICE_DISCOVERY_URL}discover/testservice', json={'service_address': mocked_service_address})
+            m.get(f'{gatewayConfig.SERVICE_DISCOVERY}discover/testservice', json={'service_address': mocked_service_address})
 
             m.get(f'{GATEWAY_URL}/testservice/somepath', json={"message": "This is a mock response"}, status_code=504)
 

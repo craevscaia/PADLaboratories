37934a1291fc6d7e9ed6cba063961a9b7afa599b
Gherman Adrian <valentina.craevscaia@isa.utm.md>
2023-10-26 00:44:38 +0300

    Unit tests for database connection and service test

---
+++ Laboratory1/BookService.Tests/BookControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using BookService.Controllers;
+using BookService.Entities;
+using BookService.Services;
+using Moq;
+
+namespace BookService.Tests;
+
+public class BookControllerTests
+{
+    [Fact]
+    public void Get_ReturnsAllBooks()
+    {
+        // Arrange
+        var mockService = new Mock<IBookService>();
+        mockService.Setup(s => s.GetAllBooks()).Returns(GetTestBooks());
+        var controller = new BookController(mockService.Object);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        var books = Assert.IsType<List<Book>>(result);
+        Assert.Equal(3, books.Count);
+    }
+
+    private List<Book> GetTestBooks()
+    {
+        return new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Author = "Author 1" },
+            new Book { Id = 2, Title = "Book 2", Author = "Author 2" },
+            new Book { Id = 3, Title = "Book 3", Author = "Author 3" },
+        };
+    }
+}
---
+++ Laboratory1/BookService.Tests/DatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿using BookService.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookService.Tests;
+
+public class DatabaseTests
+{
+    [Fact]
+    public void CanConnectToDatabase()
+    {
+        var options = new DbContextOptionsBuilder<BookContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using (var context = new BookContext(options))
+        {
+            Assert.True(context.Database.CanConnect());
+        }
+    }
+}
